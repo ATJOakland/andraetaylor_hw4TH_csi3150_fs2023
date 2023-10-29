475e23502292fffe3b8c484a412ee4d309de17e2
ATJ <145053177+ATJOakland@users.noreply.github.com>
2023-10-29 19:35:18 -0400

    Add files via upload

--- js/usedCarFilter.js
+++ js/usedCarFilter.js
@@ -1,73 +1,99 @@
 import usedCars from "./usedCars.js";
 
-// Card table and image default
-const carCardTable = document.querySelector(".carCardTable");
-const defaultCarImage = document.querySelector(".carCard img").src;
+// Function to display all cars initially
+function displayAllCars() {
+  const carCardTable = document.querySelector(".carCardTable");
+  carCardTable.innerHTML = ""; // Clear existing car cards
 
-// Hide the default carCard
-const defaultCarCard = document.querySelector(".carCard:first-child");
-defaultCarCard.style.display = "none";
+  usedCars.forEach((car) => {
+    // Create a new carCard object
+    const carCard = document.createElement("div");
+    carCard.classList.add("carCard");
 
-//---------- For showing all the cards -------------
+    // Add the cars information to the carCards
+    carCard.innerHTML = `
+      <img src="./img/older-used-car_jpg.png" alt="Picture of ${car.make} ${car.model}" style="width:100%">
+      <h1>${car.make} ${car.model}</h1>
+      <h3>Year: ${car.year}</h3>
+      <p>$${car.price}</p>
+      <p>Color: ${car.color}</p>
+      <p>Mileage: ${car.mileage}</p>
+      <p>Gas Mileage: ${car.gasMileage}</p>
+      <button>Add to Cart</button>
+    `;
 
-usedCars.forEach((car) => {
-  // Create a new carCard object
-  const carCard = document.createElement("div");
-  carCard.classList.add("carCard");
+    // Hide the no cars mesage
+    document.getElementById("noCarsMessage").style.display = "none";
 
-  // Add the same CSS attributes as the original carCard
-  carCard.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
-  carCard.style.maxWidth = "calc(33.33% - 20px)";
-  carCard.style.margin = "10px 0";
-  carCard.style.textAlign = "center";
-  carCard.style.fontFamily = "arial";
-  carCard.style.boxSizing = "border-box";
-  carCard.style.padding = "10px";
-  
-  // Add the car's information to the carCards
-  carCard.innerHTML = `
-    <img src="${defaultCarImage}" alt="Picture of ${car.make} ${car.model}" style="width:100%">
-    <h1>${car.make} ${car.model}</h1>
-    <h3>Year: ${car.year}</h3>
-    <p>$${car.price}</p>
-    <p>Color: ${car.color}</p>
-    <p>Mileage: ${car.mileage}</p>
-    <p>Gas Mileage: ${car.gasMileage}</p>
-    <button>Add to Cart</button>
-  `;
-
-  // Append the carCard element to the carCardTable element.
-  carCardTable.appendChild(carCard);
-});
-
-function applyFilter() {
-  // Get the selected color options from the dropdown.
-  const selectedColors = Array.from(document.querySelectorAll('#color option:checked')).map(option => option.value);
-
-  // Hide all car cards initially.
-  const carCards = document.querySelectorAll('.carCard');
-  carCards.forEach(carCard => {
-    carCard.style.display = 'none';
+    // Append the carCard element to the carCardTable
+    carCardTable.appendChild(carCard);
   });
-
-  // Show car cards that match the selected colors.
-  usedCars.forEach(car => {
-    if (selectedColors.includes(car.color)) {
-      // Find the  car card and show it.
-      const carCard = document.querySelector(`.carCard:has(#carColor[value="${car.color}"])`);
-      if (carCard) {
-        carCard.style.display = 'block';
-      }
-    }
-  });
-
-  console.log("Filter function called.");
 }
 
-// Makes an event for the button
+// Function to filter and display cars
+function applyFilter() {
+  const selectedMakes = Array.from(document.getElementById("make").selectedOptions).map(option => option.value);
+
+  // Filter cars based on makes
+  const filteredCars = usedCars.filter(car => selectedMakes.includes(car.make));
+
+  // Update the car cards to display the filtered
+  const carCardTable = document.querySelector(".carCardTable");
+  carCardTable.innerHTML = ""; // Clear existing car cards
+
+  filteredCars.forEach(car => {
+    // Create and append car card elements for the filtered cars
+    const carCard = document.createElement("div");
+    carCard.classList.add("carCard");
+    carCard.innerHTML = `
+      <img src="./img/older-used-car_jpg.png" alt="Picture of ${car.make} ${car.model}" style="width:100%">
+      <h1>${car.make} ${car.model}</h1>
+      <h3>Year: ${car.year}</h3>
+      <p>$${car.price}</p>
+      <p>Color: ${car.color}</p>
+      <p>Mileage: ${car.mileage}</p>
+      <p>Gas Mileage: ${car.gasMileage}</p>
+      <button>Add to Cart</button>
+    `;
+    carCardTable.appendChild(carCard);
+
+    console.log("Filter function called.");
+  });
+}
+
+// Apply Filter button
 document.getElementById("filterButton").addEventListener("click", applyFilter);
 
-/*
-If no cars from the dataset matches the filter criteria set by the user, the interface must state the same to user and ask them to try again.
-In such a scenario, no car listings should be shown on the interface.
-*/
+// Reset the filter and show all the cards
+function resetCars() {
+  const carCardTable = document.querySelector(".carCardTable");
+  carCardTable.innerHTML = ""; // Clear existing car cards
+
+  usedCars.forEach((car) => {
+    const carCard = document.createElement("div");
+    carCard.classList.add("carCard");
+
+    carCard.innerHTML = `
+      <img src="./img/older-used-car_jpg.png" alt="Picture of ${car.make} ${car.model}" style="width:100%">
+      <h1>${car.make} ${car.model}</h1>
+      <h3>Year: ${car.year}</h3>
+      <p>$${car.price}</p>
+      <p>Color: ${car.color}</p>
+      <p>Mileage: ${car.mileage}</p>
+      <p>Gas Mileage: ${car.gasMileage}</p>
+      <button>Add to Cart</button>
+    `;
+
+    carCardTable.appendChild(carCard);
+
+    document.getElementById("noCarsMessage").style.display = "none";
+
+    console.log("Reset Called.");
+  });
+}
+
+// Reset filter button
+document.getElementById("resetFilterButton").addEventListener("click", resetCars);
+
+// Display all cars initially
+displayAllCars();
